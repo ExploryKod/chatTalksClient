ebd0d65aef36c2cb6cf1f62d300de8c67c3ef7ba
Amaury Franssen <amauryfranssen@gmail.com>
2023-10-23 16:36:42 +0200

    sass and sass loader correctly configured

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sass from 'sass'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +13,12 @@ export default defineConfig({
      watch: {
        usePolling: true
      }
-  }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass,
+      },
+    },
+  },
 })
