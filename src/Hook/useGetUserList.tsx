b6613272fb723f956a93912a3ac20df7a89a18f8
Amaury Franssen <amauryfranssen@gmail.com>
2023-10-23 21:58:47 +0200

    get users is operational with bearer

--- src/Hook/useGetUserList.tsx
+++ src/Hook/useGetUserList.tsx
@@ -1,13 +1,18 @@
 import config from '../config/config.js';
+import { useLoggedStore } from '../StateManager/userStore';
 
 export default function useGetUserList() {
 
     const serverPort:number | string = config.serverPort;
-
+    const { token } = useLoggedStore();
     return function () {
         return fetch(`http://localhost:${serverPort}/user-list`, {
             method: 'GET',
-            mode: "cors"
+            mode: "cors",
+            credentials: 'same-origin',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
         })
             .then(data => data.json())
     }
