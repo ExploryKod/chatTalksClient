fc5d0554a0414d0784de6a5845ebb74e5e325dd5
Nasssdev <aissaoui_nassim@outlook.fr>
2024-04-17 02:04:32 +0200

    navigate to room from userDiscussions list, done

--- src/Pages/UserDiscussions.tsx
+++ src/Pages/UserDiscussions.tsx
@@ -2,6 +2,7 @@ import {useEffect, useState} from "react";
 import config from "../config/config.tsx";
 import {useLoggedStore} from "../StateManager/userStore.ts";
 import {IUserDiscussion} from "../Types/typeRooms";
+import {Link} from "react-router-dom";
 
 const UserDiscussions = () => {
 
@@ -25,34 +26,47 @@ const UserDiscussions = () => {
             })
             .catch(error => console.error('Error:', error));
     }, []);
-    console.log("DISCUSS", discussions)
+
+    const handleClick = async (roomId: number) => {
+        await fetch(`${serverHost}/user/discussion/delete/${userId}/${roomId}`, {
+            method: 'DELETE',
+            mode: 'cors',
+            credentials: 'same-origin',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        const newDiscussion = discussions.filter(
+            discussion => discussion.room.id !== roomId,
+        );
+        setDiscussions(newDiscussion);
+    }
+
     return (
-        // <div className="main-container">
-        //     <h1>User Discussions</h1>
-        //     <ul>
-        //         {discussions.map((discussion: any) => (
-        //             <li key={discussion.id}>
-        //                 <h2>{discussion.room.name}</h2>
-        //                 <p>{discussion.last_message.content}</p>
-        //             </li>
-        //         ))}
-        //     </ul>
-        // </div>
         <div className="main-container">
-            <h1 style={{marginLeft: "15px"}}>User Discussions</h1>
+            <h1 style={{marginLeft: "15px"}}>Mes Discussions</h1>
             <ul className="discussion-list">
                 {discussions.map((discussion: any) => (
                     <li className="discussion-item" key={discussion.id}>
                         <div className="discussion-info">
-                            <h2 className="discussion-name">{discussion.room.name}</h2>
-                            <p className="last-message">
-                                {discussion.last_message.username && (
-                                    <span style={{fontWeight: "bold"}}>
+                            <Link to={{
+                                      pathname: `/chat/${discussion.room.id.toString()}`,
+                                      search: `?id=${discussion.room.id.toString()}&name=${discussion.room.name}&description=${discussion.room.description}`,
+                                  }}>
+                                <h2 className="discussion-name">{discussion.room.name}</h2>
+                                <p className="last-message">
+                                    {discussion.last_message.username && (
+                                        <span style={{fontWeight: "bold"}}>
                                         {discussion.last_message.username}:{" "}
                                     </span>
-                                )}
-                                {discussion.last_message.content ? discussion.last_message.content : "Discussion vide, envoyez un msg en premier !"}
-                            </p>
+                                    )}
+                                    {discussion.last_message.content ? discussion.last_message.content : "Discussion vide, envoyez un msg en premier !"}
+                                </p>
+                                <span className="last-message-date">{discussion.last_message.created_at}</span>
+                            </Link>
+                            <button onClick={() => handleClick(discussion.room.id)} style={{color: "purple"}}
+                                    className={"delete-button"}>X
+                            </button>
                         </div>
                     </li>
                 ))}
